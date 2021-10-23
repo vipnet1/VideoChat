80e286bee372ad65c8838dc3143da2a5b280c344
unknown <timur.pichkhadze@gmail.com>
2021-10-23 14:35:54 +0300

    hopefully we are done

---
+++ obj/Release/Package/PackageTmp/Scripts/DeviceManagement.js
@@ -0,0 +1,95 @@
+﻿
+
+async function getConnectedDevices(type) {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    return devices.filter(device => device.kind === type)
+}
+
+
+async function InitAudioStream() { //true if success
+    try {
+        const constraints = { 'audio': { 'echoCancellation': true } };
+        const stream = await navigator.mediaDevices.getUserMedia(constraints);
+        const audioElement = document.querySelector('#localAudio');
+        audioElement.srcObject = stream;
+        console.log('init mic')
+        return true;
+    } catch (error) {
+        console.error('Error opening just mic.', error);
+    }
+    return false;
+}
+
+async function InitVideoStream() { //true if success
+    try {
+        const constraints = { 'video': true };
+        const stream = await navigator.mediaDevices.getUserMedia(constraints);
+        const videoElement = document.querySelector('#localVideo');
+        videoElement.srcObject = stream;
+        console.log('init camera')
+        return true;
+    } catch (error) {
+        console.error('Error opening video camera.', error);
+    }
+    return false;
+}
+
+function TryAddStream(pc) {
+    const localVideo = document.querySelector('#localVideo');
+    if (localVideo.srcObject !== null) {
+        localVideo.srcObject.getTracks().forEach((track) => {
+            pc.addTrack(track, localVideo.srcObject)
+        })
+    }
+    if (localAudio.srcObject !== null) {
+        const localAudio = document.querySelector('#localAudio');
+        localAudio.srcObject.getTracks().forEach((track) => {
+            pc.addTrack(track, localAudio.srcObject)
+        })
+    }
+}
+
+
+async function InitStreams(isCamOpen, isAudioOpen) { //3 - audio and video ready. 2 - just audio. 1 - just videio. 0 - nothing.
+
+    //booleans
+    const audioYES = await InitAudioStream();
+    const videoYES = await InitVideoStream();
+
+    if (videoYES) {
+        if (audioYES) {
+            return 3;
+        }
+        else
+            return 1;
+    }
+    else {
+        if (audioYES) {
+            return 2;
+        }
+        else {
+            console.log('could init nothing')
+            return 0;
+        }
+    }
+}
+
+async function disableVideo() {
+    const stream = document.querySelector('#localVideo').srcObject;
+    stream.getVideoTracks()[0].enabled = false;
+}
+
+async function playVideo() {
+    const stream = document.querySelector('#localVideo').srcObject;
+    stream.getVideoTracks()[0].enabled = true;
+}
+
+async function disableAudio() {
+    const stream = document.querySelector('#localAudio').srcObject;
+    stream.getAudioTracks()[0].enabled = false;
+}
+
+async function playAudio() {
+    const stream = document.querySelector('#localAudio').srcObject;
+    stream.getAudioTracks()[0].enabled = true;
+}
